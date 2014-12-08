c1efaf3a74419681efc25ac0b932ed335fb61753
pakoito <pakoitogod@gmail.com>
2014-12-08 22:16:50 +0000

    do all the bad practices

--- src/main/java/com/coolchick/translatortemplater/Main.java
+++ src/main/java/com/coolchick/translatortemplater/Main.java
@@ -6,14 +6,18 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private Scene mEmailSpitterScene;
 
+    private Scene mDatabaseManagerScene;
 
     @Override
     public void start(final Stage primaryStage) throws Exception {
-        final Scene scene = new Scene(new EmailSpitter().getRoot(primaryStage), 800, 600);
+        mEmailSpitterScene = new Scene(new EmailSpitter().getRoot(primaryStage), 800, 600);
+        mDatabaseManagerScene = new Scene(new DatabaseManager().getRoot(primaryStage), 800, 600);
         primaryStage.setTitle("Hello Cel");
-        primaryStage.setScene(scene);
+        primaryStage.setScene(mEmailSpitterScene);
         primaryStage.show();
+        primaryStage.setScene(mDatabaseManagerScene);
     }
 
     public static void main(String[] args) {
