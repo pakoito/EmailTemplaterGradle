475cf7ce0d9a157ae3500a5a63f868a6132515a3
Francisco Estevez <francisco.estevezgarcia@thomsonreuters.com>
2014-12-08 18:46:28 +0000

    saving as .eml file

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,3 +1,4 @@
+
 package sample;
 
 import com.coolchick.translatortemplater.model.Translator;
@@ -26,18 +27,31 @@ import javafx.stage.Stage;
 import org.controlsfx.control.ListSelectionView;
 import org.controlsfx.control.textfield.TextFields;
 
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.awt.*;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Properties;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Main extends Application {
-
     private ArrayList<Translator> mTranslators;
+
+    private HashSet<String> mLanguages;
+    private ObservableList<Translator> translatorObservableList;
+    private ObservableList<Translator> translatorsTarget;
+    private ObservableList<String> languagesObservableList;
+    private ObservableList<String> languagesTarget;
+    private TextField emailField;
 
     @Override
     public void start(final Stage primaryStage) throws Exception {
@@ -45,12 +59,13 @@ public class Main extends Application {
         fxmlLoader.setResources(ResourceBundle.getBundle("sample.theResources"));
         StackPane pane = fxmlLoader.load(this.getClass().getResource("theScene.fxml").openStream());
         mTranslators = new ArrayList<Translator>();
-        final ObservableList<Translator> translators =
-                FXCollections.observableArrayList();
-        final ObservableList<Translator> translatorsTarget =
-                FXCollections.observableArrayList();
-        final ObservableList<Node> children = ((VBox) fxmlLoader.getNamespace().get("VBox")).getChildren();
-
+        mLanguages = new HashSet<String>();
+        translatorObservableList = FXCollections.observableArrayList();
+        translatorsTarget = FXCollections.observableArrayList();
+        languagesObservableList = FXCollections.observableArrayList();
+        languagesTarget = FXCollections.observableArrayList();
+        final ObservableList<Node> children = ((VBox)fxmlLoader.getNamespace().get("VBox"))
+                .getChildren();
         GridPane filterGrid = new GridPane();
         filterGrid.setVgap(10);
         filterGrid.setHgap(10);
@@ -63,7 +78,8 @@ public class Main extends Application {
             public void handle(KeyEvent event) {
                 switch (event.getCode()) {
                     case ENTER:
-                        getTranslatorsForLanguage(textField, translators, translatorsTarget);
+                        getTranslatorsForFilter(textField, translatorObservableList, translatorsTarget);
+                        geLanguageForFilter(textField, languagesObservableList, languagesTarget);
                         break;
                     default:
                         break;
@@ -71,59 +87,122 @@ public class Main extends Application {
             }
         });
         GridPane.setHgrow(textField, Priority.ALWAYS);
-
         final ListSelectionView<Translator> listSelectionView = new ListSelectionView<Translator>();
-        listSelectionView.setSourceItems(translators);
+        listSelectionView.setSourceItems(translatorObservableList);
         listSelectionView.setTargetItems(translatorsTarget);
-
-        final FileChooser fileChooser = new FileChooser();
-
-        final javafx.scene.control.Button openButton = new javafx.scene.control.Button("Load JSON database...");
-        openButton.setOnAction(
-                new EventHandler<ActionEvent>() {
-                    @Override
-                    public void handle(final ActionEvent e) {
-                        File file = fileChooser.showOpenDialog(primaryStage);
-                        if (file != null) {
-                            ObjectMapper mapper = new ObjectMapper();
-                            try {
-                                TranslatorDatabase database = mapper.readValue(file, TranslatorDatabase.class);
-                                HashSet<String> languages = new HashSet<String>();
-                                for (Translator translator : database.getTranslators()) {
-                                    mTranslators.add(translator);
-                                    for (String language : translator.getLanguages()) {
-                                        languages.add(language);
-                                    }
-                                }
-                                TextFields.bindAutoCompletion(textField, languages);
-                                getTranslatorsForLanguage(textField, translators, translatorsTarget);
-                            } catch (IOException e1) {
-                                showErrorDialog(primaryStage, "Bad translator database");
+        final ListSelectionView<String> languagesSelection = new ListSelectionView<String>();
+        languagesSelection.setSourceItems(languagesObservableList);
+        languagesSelection.setTargetItems(languagesTarget);
+        final javafx.scene.control.Button openButton = new javafx.scene.control.Button(
+                "Load JSON database...");
+        openButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(final ActionEvent e) {
+                FileChooser fileChooser = new FileChooser();
+                fileChooser.setTitle("Choose your JSON database");
+                File file = fileChooser.showOpenDialog(primaryStage);
+                if (file != null) {
+                    ObjectMapper mapper = new ObjectMapper();
+                    try {
+                        TranslatorDatabase database = mapper.readValue(file,
+                                TranslatorDatabase.class);
+                        for (Translator translator : database.getTranslators()) {
+                            mTranslators.add(translator);
+                            for (String language : translator.getLanguages()) {
+                                mLanguages.add(language);
                             }
                         }
+                        TextFields.bindAutoCompletion(textField, mLanguages);
+                        getTranslatorsForFilter(textField, translatorObservableList, translatorsTarget);
+                        geLanguageForFilter(textField, languagesObservableList, languagesTarget);
+                    } catch (IOException e1) {
+                        showErrorDialog(primaryStage, "Bad translator database");
                     }
-                });
-        children.add(
-                openButton
-        );
+                }
+            }
+        });
+        GridPane emailGrid = new GridPane();
+        emailGrid.setVgap(10);
+        emailGrid.setHgap(10);
+        emailGrid.setPadding(new Insets(30, 30, 0, 30));
+        emailField = new TextField();
+        emailGrid.add(new Label("From Email"), 0, 0);
+        emailGrid.add(emailField, 1, 0);
+        final javafx.scene.control.Button spitButton = new javafx.scene.control.Button(
+                "Spit email...");
+        spitButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(final ActionEvent e) {
+                FileChooser fileChooser = new FileChooser();
+                fileChooser.setTitle("Choose destination");
+                fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Email draft(*.eml)", "*.eml"));
+                File file = fileChooser.showSaveDialog(primaryStage);
+                if (file != null) {
+                    try {
+                        Properties properties = System.getProperties();
+                        Session session = Session.getDefaultInstance(properties);
+                        MimeMessage message = new MimeMessage(session);
+                        final Address[] addresses = new Address[translatorsTarget.size()];
+                        for (int i = 0; i < translatorsTarget.size(); i++) {
+                            Translator trans = translatorsTarget.get(i);
+                            addresses[i] = new InternetAddress(trans.getEmail());
+                        }
+                        String from = emailField.getText();
+                        if (from != null && !"".equalsIgnoreCase(from)){
+                            message.addFrom(new Address[]{new InternetAddress()});
+                        }
+                        message.addRecipients(Message.RecipientType.BCC, addresses);
+                        message.addHeaderLine("X-Unsent: 1");
+                        message.setSubject("Sending email for languages: " + languagesTarget);
+                        message.setContent("<h1>This is actual message</h1>", "text/html");
+                        message.writeTo(new FileOutputStream(file));
+                        openFile(file);
+                    } catch (MessagingException e1) {
+                        showErrorDialog(primaryStage, "Failed to open file\n" + e1);
+                    } catch (FileNotFoundException e1) {
+                        showErrorDialog(primaryStage, "Failed to open file\n" + e1);
+                    } catch (IOException e1) {
+                        showErrorDialog(primaryStage, "File type unknown, please open it externally");
+                    }
+                }
+            }
+        });
+        children.add(openButton);
         children.add(filterGrid);
-        children.add(
-                listSelectionView
-        );
-        primaryStage.setTitle("Hello World");
+        children.add(listSelectionView);
+        children.add(languagesSelection);
+        children.add(emailGrid);
+        children.add(spitButton);
+        primaryStage.setTitle("Hello Cel");
         final Scene scene = new Scene(pane);
-        scene.getStylesheets().setAll(
-                getClass().getResource("theStyles.css").toExternalForm());
+        scene.getStylesheets().setAll(getClass().getResource("theStyles.css").toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.sizeToScene();
         primaryStage.show();
     }
 
-    private void getTranslatorsForLanguage(TextField textField, ObservableList<Translator> translators, ObservableList<Translator> translatorsTarget) {
+    private void getTranslatorsForFilter(TextField textField,
+            ObservableList<Translator> translators, ObservableList<Translator> translatorsTarget) {
         translators.clear();
         for (Translator translator : mTranslators) {
-            if (textField.getText() == null || textField.getText().equalsIgnoreCase("") || (translator.getLanguages().contains(textField.getText()) && !translatorsTarget.contains(translator))) {
+            if (textField.getText() == null
+                    || textField.getText().equalsIgnoreCase("")
+                    || (translator.getLanguages().contains(textField.getText()) && !translatorsTarget
+                            .contains(translator))) {
                 translators.add(translator);
+            }
+        }
+    }
+
+    private void geLanguageForFilter(TextField textField, ObservableList<String> languages,
+            ObservableList<String> languagesTarget) {
+        languages.clear();
+        for (String language : mLanguages) {
+            if (textField.getText() == null
+                    || textField.getText().equalsIgnoreCase("")
+                    || (textField.getText().equalsIgnoreCase(language) && !languagesTarget
+                            .contains(language))) {
+                languages.add(language);
             }
         }
     }
@@ -137,17 +216,9 @@ public class Main extends Application {
         dlg.show();
     }
 
-    private void openFile(File file) {
-        try {
+    private void openFile(File file) throws IOException {
             Desktop.getDesktop().open(file);
-        } catch (IOException ex) {
-            Logger.getLogger(
-                    getClass().getName()).log(
-                    Level.SEVERE, null, ex
-            );
-        }
     }
-
 
     public static void main(String[] args) {
         launch(args);
