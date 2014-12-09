f6a18171a9c6d36a582df81c81bc3bd071bb5691
pakoito <pakoitogod@gmail.com>
2014-12-09 02:15:25 +0000

    save dialogs etc

--- src/main/java/com/coolchick/translatortemplater/PersonOverviewController.java
+++ src/main/java/com/coolchick/translatortemplater/PersonOverviewController.java
@@ -11,22 +11,24 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import org.controlsfx.control.textfield.TextFields;
 import org.controlsfx.dialog.Dialogs;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.io.*;
+import java.util.*;
 
 public class PersonOverviewController {
     private HashSet<String> mLanguages = new HashSet<String>();
+
+    private ArrayList<Translator> mTranslators = new ArrayList<Translator>();
 
     private ObservableList<Translator> translatorObservableList = FXCollections
             .observableArrayList();
@@ -60,6 +62,13 @@ public class PersonOverviewController {
 
     // Reference to the main application.
     private Main mainApp;
+
+    @FXML
+    private GridPane filterGrid;
+
+    private TextField languageFilter;
+
+    private TextField nameFilter;
 
     /**
      * The constructor. The constructor is called before the initialize() method.
@@ -107,7 +116,53 @@ public class PersonOverviewController {
         returnButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(final ActionEvent e) {
-                mainApp.showMailSpitter();
+				if (mTranslators.size() > 0) {
+					showExitDialog();
+				} else {
+					mainApp.showMailSpitter();
+				}
+            }
+        });
+        final javafx.scene.control.Button spitButton = new javafx.scene.control.Button(
+                "Save database...");
+        filterGrid.setPadding(new Insets(10, 10, 10, 10));
+        filterGrid.setVgap(10);
+        filterGrid.setHgap(10);
+        languageFilter = new TextField();
+        nameFilter = new TextField();
+        filterGrid.add(new Label("Name filter"), 0, 0);
+        filterGrid.add(nameFilter, 1, 0);
+        filterGrid.add(new Label("Language filter"), 0, 1);
+        filterGrid.add(languageFilter, 1, 1);
+        filterGrid.add(spitButton, 4, 1);
+        spitButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(final ActionEvent e) {
+                spitDatabase();
+            }
+        });
+        languageFilter.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                switch (event.getCode()) {
+                    case ENTER:
+                        getTranslatorsForFilter(languageFilter.getText());
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+        nameFilter.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                switch (event.getCode()) {
+                    case ENTER:
+                        getTranslatorsForName(nameFilter.getText());
+                        break;
+                    default:
+                        break;
+                }
             }
         });
     }
@@ -195,10 +250,11 @@ public class PersonOverviewController {
         try {
             TranslatorDatabase database = mapper.readValue(file, TranslatorDatabase.class);
             mLanguages.clear();
+            mTranslators.clear();
             translatorObservableList.clear();
             Set<String> names = new HashSet<String>();
             for (Translator translator : database.getTranslators()) {
-                translatorObservableList.add(translator);
+                mTranslators.add(translator);
                 names.add(translator.getName());
                 for (String language : translator.getLanguages()) {
                     mLanguages.add(language);
@@ -206,9 +262,11 @@ public class PersonOverviewController {
             }
             mLanguages.addAll(database.getAllLanguages());
             personTable.setItems(translatorObservableList);
-            // TextFields.bindAutoCompletion(languageFilter, mLanguages);
-            // TextFields.bindAutoCompletion(nameFilter, names);
-            // getTranslatorsForFilter("", translatorObservableList, translatorsTarget);
+            translatorObservableList.addAll(mTranslators);
+            languageFilter.setText("");
+            nameFilter.setText("");
+            TextFields.bindAutoCompletion(languageFilter, mLanguages);
+            TextFields.bindAutoCompletion(nameFilter, names);
         } catch (IOException e1) {
             showErrorDialog(mainApp.getStage(), "Bad translator database");
         }
@@ -223,12 +281,113 @@ public class PersonOverviewController {
         dlg.show();
     }
 
-    private void showOkayDialog(Stage primaryStage, String text) {
-        Alert dlg = new Alert(Alert.AlertType.INFORMATION);
-        dlg.setTitle("OK.WEBP");
-        dlg.initOwner(primaryStage);
-        dlg.initModality(Modality.WINDOW_MODAL);
-        dlg.getDialogPane().setContentText(text);
-        dlg.show();
+    private void showExitDialog() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Leaving database editor!");
+        alert.setHeaderText("Do you want to save before leaving?");
+        alert.setContentText("Choose your option.");
+        ButtonType buttonTypeOne = new ButtonType("Save and leave");
+        ButtonType buttonTypeTwo = new ButtonType("Leave without saving");
+        ButtonType buttonTypeCancel = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
+        alert.getButtonTypes().setAll(buttonTypeOne, buttonTypeTwo, buttonTypeCancel);
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == buttonTypeOne) {
+            spitDatabase();
+            mainApp.showMailSpitter();
+        } else if (result.get() == buttonTypeTwo) {
+            mainApp.showMailSpitter();
+        } else {
+            // ... user chose CANCEL or closed the dialog
+        }
     }
+
+    private void getTranslatorsForName(String text) {
+        translatorObservableList.clear();
+        for (Translator translator : mTranslators) {
+            if (text == null
+                    || text.equalsIgnoreCase("")
+                    || (translator.getName().replace(" ", "").toLowerCase().contains(text.replace(
+                            " ", "").toLowerCase()))) {
+                translatorObservableList.add(translator);
+            }
+        }
+        translatorObservableList.sort(new Comparator<Translator>() {
+            @Override
+            public int compare(Translator o1, Translator o2) {
+                int compare = o1.getName().compareToIgnoreCase(o2.getName());
+                if (compare < 0) {
+                    return -1;
+                } else if (compare > 0) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
+        });
+    }
+
+    private void getTranslatorsForFilter(String textField) {
+        translatorObservableList.clear();
+        for (Translator translator : mTranslators) {
+            if (textField == null || textField.equalsIgnoreCase("")
+                    || (translator.getLanguages().contains(textField))) {
+                translatorObservableList.add(translator);
+            }
+        }
+        translatorObservableList.sort(new Comparator<Translator>() {
+            @Override
+            public int compare(Translator o1, Translator o2) {
+                int compare = o1.getName().compareToIgnoreCase(o2.getName());
+                if (compare < 0) {
+                    return -1;
+                } else if (compare > 0) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
+        });
+    }
+
+    private void spitDatabase() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Choose destination");
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("JSON file(*.json)", "*.json"));
+        File file = fileChooser.showSaveDialog(mainApp.getStage());
+        if (file != null) {
+            try {
+                ObjectMapper mapper = new ObjectMapper();
+                Set<String> languages = new HashSet<String>();
+                languages.addAll(mLanguages);
+                for (Translator translator : translatorObservableList) {
+                    for (String language : translator.getLanguages()) {
+                        languages.add(language);
+                    }
+                }
+                TranslatorDatabase database = new TranslatorDatabase().withAllLanguages(
+                        new ArrayList<String>(languages)).withTranslators(translatorObservableList);
+                String databaseSerialized = mapper.writeValueAsString(database);
+                FileWriter fw = new FileWriter(file.getAbsoluteFile());
+                BufferedWriter bw = new BufferedWriter(fw);
+                bw.write(databaseSerialized);
+                bw.close();
+                loadDatabase(file);
+				showInformation("Database save", "Saved correctly!");
+            } catch (FileNotFoundException e1) {
+                showErrorDialog(mainApp.getStage(), "Failed to open file\n" + e1);
+            } catch (IOException e1) {
+                showErrorDialog(mainApp.getStage(), "File type unknown, please open it externally");
+            }
+        }
+    }
+
+	private void showInformation(String title, String text) {
+		Alert alert = new Alert(Alert.AlertType.INFORMATION);
+		alert.setTitle(title);
+		alert.setHeaderText(null);
+		alert.setContentText(text);
+
+		alert.showAndWait();
+	}
 }
